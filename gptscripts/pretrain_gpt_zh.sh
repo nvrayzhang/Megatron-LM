c8c76e3c0ef58d69a6e83a0203a1ab24927a87d3
nvrayzhang <rayzhang@nvidia.com>
2022-04-22 18:15:50 +0800

    rewrite pretrain scripts

--- gptscripts/pretrain_gpt_zh.sh
+++ gptscripts/pretrain_gpt_zh.sh
@@ -2,47 +2,28 @@
 # Runs the "345M" parameter model
 IMAGE=pytorch2203
 
-
+# ROOT DIR OF MEGATRON
 MEGATRON=/home/nvidia/Projects/Megatron-LM
-VOCAB=${MEGATRON}/vocab/jq.zh.v2.vocab
-DATA_PATH=${MEGATRON}/data/oscar_text_document
-
 EXE=${MEGATRON}/pretrain_gpt_zh.py
-
+VOCAB_PATH=${MEGATRON}/vocab/jq.zh.v2.vocab
+DATA_PATH=${MEGATRON}/data/oscar_text_document
 CHECKPOINT_PATH=/data/checkpoint
-mkdir -p ${CHECKPOINT_PATH}
 LOG_PATH=${CHECKPOINT_PATH}/log
+LOGFILE=${CHECKPOINT_PATH}/cmdlog.log
 
-
-
-NUM_LAYERS=10
-HIDDEN_SIZE=512
-NUM_ATTENTION_HEADS=4
-MICRO_BATCH_SIZE=4
-GLOBAL_BATCH_SIZE=8
-SEQ_LENGTH=1024
-MAX_POSITION_EMBEDDINGS=1024
-TRAIN_ITERS=50000
-LR_DECAY_ITERS=30000
-
-"
-       --num_layers $NUM_LAYERS \
-       --hidden-size $HIDDEN_SIZE \
-       --num-attention-heads $NUM_ATTENTION_HEADS \
-       --micro-batch-size $MICRO_BATCH_SIZE \
-       --global-batch-size $GLOBAL_BATCH_SIZE \
-       --seq-length $SEQ_LENGTH \
-       --max-position-embeddings $MAX_POSITION_EMBEDDINGS \
-       --train-iters $TRAIN_ITERS \
-       --lr-decay-iters $LR_DECAY_ITERS \
-"
-
+# DISTRIBUTED SETTINGS
 MASTER_ADDR=localhost
-MASTER_PORT=7000
+MASTER_PORT=9980
 NNODES=1
 NODE_RANK=0
 GPUS_PER_NODE=2
-WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+WORLD_SIZE=$(($GPUS_PER_NODE * $NNODES))
+
+# TRAINING SETTINGS
+LR=0.00015
+ITERS=50000
+SAVE_INTERVAL=10000
+EVAL_INTERVAL=1000
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE \
                   --nnodes $NNODES \
@@ -50,35 +31,42 @@ DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE \
                   --master_addr $MASTER_ADDR \
                   --master_port $MASTER_PORT"
 
-docker exec ${IMAGE} bash -c "cd ${MEGATRON}; \
-echo MASTER_ADDR=${MASTER_ADDR}; \
-echo MASTER_PORT=${MASTER_PORT}; \
-torchrun  ${EXE} \
-       --num-layers 6 \
-       --hidden-size 512 \
-       --num-attention-heads 4 \
+OUTPUT_ARGS="--log-interval 100 \
+              --tensorboard-dir ${LOG_PATH} \
+              --tensorboard-log-interval 50 \
+              --no-log-loss-scale-to-tensorboard \
+              --log-interval 100 \
+              --save-interval ${SAVE_INTERVAL} \
+              --eval-interval ${EVAL_INTERVAL} \
+              --eval-iters 10 \
+              --activations-checkpoint-method uniform \
+              --save ${CHECKPOINT_PATH} \
+              --load ${CHECKPOINT_PATH}"
+
+docker exec ${IMAGE} bash -c "cd ${MEGATRON}; mkdir ${CHECKPOINT_PATH}; mkdir ${LOG_PATH}; \
+python -m torch.distributed.launch ${DISTRIBUTED_ARGS} \
+       ${EXE} \
+       ${TENSORBOARD_ARGS} \
+       --tensor-model-parallel-size 2 \
+       --pipeline-model-parallel-size 2 \
+       --num-layers 24 \
+       --hidden-size 1024 \
+       --num-attention-heads 16 \
        --micro-batch-size 4 \
-       --global-batch-size 8 \
-       --seq-length 512 \
-       --max-position-embeddings 512 \
-       --train-iters 50000 \
-       --lr-decay-iters 32000 \
-       --save ${CHECKPOINT_PATH} \
-       --load ${CHECKPOINT_PATH} \
-       --data-path ${DATA_PATH} \
-       --vocab-file ${VOCAB} \
+       --global-batch-size 16 \
+       --seq-length 1024 \
+       --max-position-embeddings 1024 \
+       --train-iters ${ITERS} \
+       --lr-decay-iters 320000 \
+       --data-path $DATA_PATH \
+       --vocab-file $VOCAB_PATH \
        --data-impl mmap \
        --split 949,50,1 \
        --distributed-backend nccl \
-       --lr 0.00015 \
-       --min-lr 1.0e-5 \
+       --lr ${LR} \
        --lr-decay-style cosine \
+       --min-lr 1.0e-5 \
        --weight-decay 1e-2 \
        --clip-grad 1.0 \
        --lr-warmup-fraction .01 \
-       --activations-checkpoint-method uniform \
-       --log-interval 100 \
-       --save-interval 10000 \
-       --eval-interval 1000 \
-       --eval-iters 10 \
-       --fp16 "
+       --fp16 | tee $LOGFILE"
